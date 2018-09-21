19bd01d7a03745bcbc299d274047a4c524bd087d
jaya-yellowant <jaya@yellowant.com>
2018-09-21 12:53:58 +0530

    Changed the HTML for the site.

--- blog/models.py
+++ blog/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 from django.utils import timezone
 
-
+# Create your models here.
 class Post(models.Model):
     author = models.ForeignKey('auth.User', on_delete=models.CASCADE)
     title = models.CharField(max_length=200)
@@ -17,4 +17,4 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
-# Create your models here.
+
